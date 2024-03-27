d420e34e5b47b6f2059f08c37867e17956045c73
Antonio <sertic43@gmail.com>
2024-03-27 12:45:41 +0100

    Implementing Pagination

--- src/js/model.js
+++ src/js/model.js
@@ -7,8 +7,8 @@ export const state = {
   search: {
     query: '',
     results: [],
-    /*     page: 1,
-    resultsPerPage: RES_PER_PAGE, */
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -52,4 +52,13 @@ export const loadSearchResults = async function (query) {
   } catch (err) {
     console.error(`${err} 💥💥💥💥`);
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; // 0
+  const end = page * state.search.resultsPerPage; // 9
+
+  return state.search.results.slice(start, end);
 };
